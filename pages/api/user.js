e457e2448fd62cd78f65363e3cb4b1e5f2d92f5f
hopeman1102 <hopeman1102@gmail.com>
2023-06-10 13:10:02 +0900

    update schema

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,5 +1,22 @@
 import connectDB from '../../middleware/mongodb';
-import Project from '../../models/project';
+
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.ObjectId;
+
+const ProjectSchema = new mongoose.Schema(
+{
+    category: { type: ObjectId, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    images: [{ type: String }],
+    skills: [{ type: String }],
+    url: { type: String, default: '' },
+    github: { type: String, default: '' }
+});
+
+mongoose.models = {};
+
+const Project = mongoose.model('Project', ProjectSchema);
 
 const handler = async (req, res) => {
   const projects = await Project.find({});
